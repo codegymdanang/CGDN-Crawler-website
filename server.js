69dd0518827c44db8b744c195c87bdd168e5f777
codegymdanang <codegymdanang@gmail.com>
2019-08-29 17:23:54 +0700

    get app store links

--- server.js
+++ server.js
@@ -4,41 +4,34 @@ const fs = require('fs');
 
 
 
-function crawlerPopularLinks()
+function crawlerPopularLinks(link)
 {
-    //Gửi 1 request tới website
-    request('https://apps.apple.com/kn/genre/ios-business/id6000?letter=A', function (err, res, body)
-    {
-        //  Sử dụng cheerio.load để lấy dữ liệu trả về
-        var $ = cheerio.load(body);
-        //  get select content
-        var links = new Array();
+    var popularLinks = new Array();
+    return new Promise((resolve,reject) => {
+        request(link, function (err, res, body)
+        {
+            console.log("Source Link" , link );
+            //  Sử dụng cheerio.load để lấy dữ liệu trả về
+            var $ = cheerio.load(body);
+            //  get select content
 
-        $('#selectedcontent').find('a').each(function(i,elem){
-            links.push($(this).attr('href'));
-        });
 
-        var json = JSON.stringify(links);
-        //  Kiểm tra file newchap.json tồn tại không
-        if (!fs.existsSync('popularapp.json')) {
-            //  Nếu chưa tồn tại tạo file  mới
-            fs.writeFile('popularapp.json', json, '', (err)=>{
-                if (err) throw err;
-                console.log('created popularapp.json !');
+            $('#selectedcontent').find('a').each(function(i,elem){
+                popularLinks.push($(this).attr('href'));
             });
-            return;
-        }
-
+            resolve(popularLinks);
+        })
     })
+
 }
 
 
 
 function crawlerAppSupportLink(appSupportlink) {
     let link = "";
-    return new Promise(resolve => {
+    return new Promise((resolve,reject) => {
         request(appSupportlink, function (err, res, body) {
-            //  Sử dụng cheerio.load để lấy dữ liệu trả về
+
             let $ = cheerio.load(body);
             //  get select content
             let category = $('.link').filter(function() {
@@ -54,7 +47,7 @@ function crawlerAppSupportLink(appSupportlink) {
 
 }
 
-function readPopularLinks() {
+async function readAppSupportLinksFromFile() {
     let appSupportLinks = new Array();
     return new Promise((resolve, reject)=>{
         fs.readFile('popularapp.json', async function readFileCallback(err, data) {
@@ -64,8 +57,10 @@ function readPopularLinks() {
             } else {
                 // get all popular link from file
                 let popularLinks = JSON.parse(data);
-                for (var index in popularLinks) {
-                    var link = await crawlerAppSupportLink(popularLinks[index]);
+
+                for(let index in popularLinks) {
+                    console.log("Link" +index+ " is" + popularLinks[index] + "\n");
+                    let link = await crawlerAppSupportLink(popularLinks[index]);
                     console.log("Link " , link);
                     appSupportLinks.push(link);
 
@@ -78,7 +73,21 @@ function readPopularLinks() {
 
 }
 
-function saveAppSupportLinkToFile(appSupportLinks) {
+function savePopularlinksToFile(links) {
+    console.log(links);
+    var json = JSON.stringify(links);
+    //  Kiểm tra file newchap.json tồn tại không
+    if (!fs.existsSync('popularapp.json')) {
+        //  Nếu chưa tồn tại tạo file  mới
+        fs.writeFile('popularapp.json', json, '', (err)=>{
+            if (err) throw err;
+            console.log('created popularapp.json !');
+        });
+        return;
+    }
+}
+
+function saveAppSupportLinksToFile(appSupportLinks) {
     var json = JSON.stringify(appSupportLinks);
 
     if (!fs.existsSync('appsupport.json')) {
@@ -91,13 +100,34 @@ function saveAppSupportLinkToFile(appSupportLinks) {
     }
 }
 
-//crawlerPopularLinks();
-//readPopularLinks();
-//saveAppSupportLinkToFile();
+function readSourceLinksFromFile() {
+    let appLinks = new Array();
+    return new Promise((resolve, reject)=>{
+        fs.readFile('sourcelink.json', async function readFileCallback(err, data) {
+            if (err) {
+                console.log('Fail to  popularapp.json !');
+                return;
+            } else {
+                // get all popular link from file
+                let sourceLinks = JSON.parse(data);
+                for (var index in sourceLinks) {
+                    var link = await crawlerPopularLinks(sourceLinks[index]);
+                    console.log("=== Popular Link ===" , link);
+                    appLinks.push(...link);
+
+                }
+                resolve(appLinks);
+            }
+        });
+    });
+}
+
 
 async function crawlData() {
-    const appSupport = await readPopularLinks();
-    saveAppSupportLinkToFile(appSupport);
+    const popularlink = await readSourceLinksFromFile();
+    await savePopularlinksToFile(popularlink);
+    const appSupport = await readAppSupportLinksFromFile();
+    saveAppSupportLinksToFile(appSupport);
 }
 
 crawlData();
